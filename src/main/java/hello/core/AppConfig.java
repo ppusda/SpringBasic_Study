fe9391766fed106ab19eb0ef25dde2a461ad03cd
ppusda <ppusda1234@gmail.com>
2021-06-03 23:51:46 +0900

    할인제도 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,12 +14,21 @@ public class AppConfig { // 앱의 기획자 역활, 의존관계 주입을 할 
     // 역할과 책임을 적절하게 분리하는데 사용했음!
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
 
-    // Ctrl + E - 과거 히스토리 다 보여줌줌
+    public MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+    // Ctrl + E - 과거 히스토리 다 보여줌
 }
