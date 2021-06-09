ee32c11b3ea007251840207e3cd836e68fab5031
ppusda <ppusda1234@gmail.com>
2021-06-09 23:23:57 +0900

    싱글톤 구현해보기

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+    // static으로 생성해서 클래스영역(Static 영역)에 하나만 만들어두고
+
+    public static SingletonService getInstance() {
+        return instance;
+    } // 해당 메소드를 통해서만 조회 할 수 있고,
+    // 이 메서드를 호출하면 항상 같은 인스턴스를 반환한다.
+
+    private SingletonService() { } // private 외부 생성 막기
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+
+    // 객체를 미리 생성해두는 가장 단순하고 안전한 방법
+    // 다른 방법도 있다.
+}
+
+// 싱글톤의 문제점들도 있다.
+/*
+   싱글톤 패턴을 구현하는 코드가 필요하다.
+   의존관계상 클라이언트가 구체 클래스에 의존한다 = DIP 위반
+   OCP를 위반할 가능성이 있다.
+   테스트하기 힘들다.
+   유연성이 떨어진다
+   private 이기에 자식 클래스를 만들기 어렵다
+   안티패턴으로 불리기도 한다.
+ */
--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
@@ -22,7 +24,23 @@ public class SingletonTest {
         System.out.println("memberService = " + memberService2);
 
         //memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
     } // 이렇게 하면 요청이 들어올 때 마다 객체가 생성이 된다.
-    // 정말 비효율적일 텐데, 이를 해결 할 방법은 구현체를 한 개만 생성시키고 공유를 하면 된다.
+    // 정말 비효율적일 텐데, 이를 해결 할 방법은 구현체를 한 개만 생성시키고 공유를 하면 된다 = 싱글톤 패턴
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isEqualTo(singletonService2);
+        // same, 메모리를 비교
+        // equal, 값 자체를 비교
+    }
+    // 이렇게 싱글톤 패턴을 사용할 수 있다.
+    // 하지만 스프링 컨테이너에서는 싱글톤을 직접 관리해주는 역할을 한다.
 }
