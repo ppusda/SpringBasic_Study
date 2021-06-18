aa8f7ef72e29c06c71b7a03fcd274d7a463bce86
ppusda <ppusda1234@gmail.com>
2021-06-18 22:46:38 +0900

    빈 스코프에 대해서

---
+++ src/test/java/hello/core/scope/ProtorypeTest.java
@@ -0,0 +1,47 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProtorypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
+
+// 프로토타입 스코프 빈은 컨테이너에서 빈을 조회할 때 생성되고, 초기화 메서드도 실행된다.
+// @PreDestroy가 실행되지 않는다는 것, 종료 메서드를 사용하고 싶다면 일일히 종료시켜야만 한다.
+
+// ====== 클라이언트가 직접 관리해야한다...
